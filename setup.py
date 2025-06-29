04f8fb42da67e77e15e716750e2f716316243268
Pritush kr Singh <mynameispritush.15@gmail.com>
2025-06-29 13:48:54 +0530

    understood -e . usage and .toml and setup as well

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Pritush Kr Singh",
+    author_email="mynameispritush.15@gmail.com",
     packages=find_packages()
 )
